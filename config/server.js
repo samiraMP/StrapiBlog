086b58a2cee76106f7f16fee98b9762f379873b2
Samira <samira@Samiras-MacBook-Pro.local>
2022-01-02 09:03:40 +0330

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8e6006cd07a21a5539cab51e191d73f6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4083fd97-881e-4bfa-878c-6e73e09eb9ed'
+};
